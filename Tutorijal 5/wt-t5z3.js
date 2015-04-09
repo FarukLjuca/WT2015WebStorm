1d87c6e5d42a1cf84be2ad7a216723432a7795de
Faruk Ljuca <fljuca1@etf.unsa.ba>
2015-04-09 20:17:15 +0200

    uklonio jquery

--- Tutorijal 5/wt-t5z3.js
+++ Tutorijal 5/wt-t5z3.js
@@ -1,4 +1,6 @@
+/*
 setInterval(function(){
+
     var ajax =  new XMLHttpRequest();
 
     ajax.onreadystatechange = function() {
@@ -16,4 +18,39 @@ setInterval(function(){
     var grad = document.getElementById("grad").value;
     ajax.open("GET", "http://www.klix.ba/rss/naslovnica", true);
     ajax.send();
+
+    $.ajax({
+        url      : document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent('http://stackoverflow.com/feeds'),
+        dataType : 'json',
+        success  : function (data) {
+            if (data.responseData.feed && data.responseData.feed.entries) {
+                var tekst = "";
+                $.each(data.responseData.feed.entries, function (i, e) {
+                    alert(tekst);
+                    tekst += "------------------------";
+                    tekst += "Naslov: " + e.title;
+                    tekst += "Link: " + e.id;
+                });
+                document.getElementsByTagName("body").innerHTML = tekst;
+            }
+        }
+    });
 }, 1);
+*/
+$(document).ready(function() {
+    $.ajax({
+        url      : document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent('http://stackoverflow.com/feeds'),
+        dataType : 'json',
+        success  : function (data) {
+            if (data.responseData.feed && data.responseData.feed.entries) {
+                var tekst = "";
+                $.each(data.responseData.feed.entries, function (i, e) {
+                    alert(tekst);
+                    tekst += "<h3>" + e.title + "</h3>";
+                    tekst += "<p>Pitanje se nalazi na <a href='" + e.id + "'>ovom</a> linku</p>";
+                });
+                document.getElementById("tekst").innerHTML = tekst;
+            }
+        }
+    });
+});
